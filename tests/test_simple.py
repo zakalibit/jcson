57d269c3482eadfd75db6886bf2a8d8425f8c841
Alex Revetchi <alex.revetchi@synchronoss@com>
2020-03-20 17:36:19 +0000

    Fix test jcson file path

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
 
+import os
 import jcson
 
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+
 def test_simple_subst():
-    m = jcson.read('simple.jcson')
+    m = jcson.read(os.path.join(CURDIR, 'simple.jcson'))
     assert m['services']['proxy']['port'] == 80
     assert m['services']['webapp']['port'] == 8080

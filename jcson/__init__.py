9e3a32d4b1769ed37e24eb1b294d1e24e470ad32
Alex Revetchi <alex.revetchi@synchronoss@com>
2020-04-12 11:22:08 +0100

    - add reads() loading from string - unitests for reads()

--- jcson/__init__.py
+++ jcson/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.4'
-__all__ = ['read']
+__version__ = '0.1.5'
+__all__ = ['read', 'reads']
 
 __author__ = 'Alex Revetchi <alex.revetchi@gmail.com>'
 
@@ -66,10 +66,8 @@ def file_content(filename):
         return content
         
     
-def process_includes(filename):
-    path = os.path.dirname(os.path.abspath(filename))
-
-    fcontent = file_content(filename)
+def process_includes(fcontent, include_path=None):
+    path = include_path or os.getcwd()
     includes = [i for i in re_include.finditer(fcontent)]
 
     while len(includes):
@@ -87,10 +85,6 @@ def process_includes(filename):
         fcontent = content
         
     return fcontent
-
-
-def load_content(filename):
-    return process_includes(filename)
 
 
 # ${var.name} substitution with in the file or env
@@ -135,8 +129,14 @@ def expand_node_substitution(config, subst, path, value):
             config[p] = config[p].replace(var, str(value))
 
 
-def read(filename):
-    content = load_content(filename)
+def read(filename, include_path=None):
+    include_path = include_path or os.path.dirname(os.path.abspath(filename))
+    content = file_content(filename)
+    return reads(content, include_path=include_path)
+
+
+def reads(content, include_path=None):
+    content = process_includes(content, include_path)
     config = json.loads(content, object_pairs_hook=OrderedDict)
 
     while re_var.findall(json.dumps(config)):
@@ -153,5 +153,4 @@ def read(filename):
             for s in subs:
                 v = resolve_substitution_value(config, s['var'], s['path'])
                 expand_node_substitution(config, s['var'], s['path'], v)
-
     return config
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -10,3 +10,40 @@ def test_simple_subst():
     assert m['services']['proxy']['port'] == 80
     assert m['services']['webapp']['port'] == 8080
     assert m['services']['webapp']['env']['debug'] == 1
+
+
+config = """
+{
+    include "simple_include.jcson"
+
+    "services": {
+        "proxy": {
+            "port": "${profile.ports.proxy}"
+        },
+
+        "webapp": {
+            "port": "${profile.ports.webapp}",
+            "env": "${profile.env}"
+        }
+    }
+}
+"""
+
+def test_reads():
+    m = jcson.reads(config, include_path = CURDIR)
+    assert m['services']['proxy']['port'] == 80
+    assert m['services']['webapp']['port'] == 8080
+    assert m['services']['webapp']['env']['debug'] == 1
+
+
+
+config_env = """
+{
+    "user": "${USER}"
+}
+"""
+
+def test_env_susbst():
+    user = os.environ['USER']
+    m = jcson.reads(config_env, include_path = CURDIR)
+    assert m['user'] == user
